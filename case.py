f73c4838ed90163c1f51abe03da5c2683ff39f91
Oleh Synyshyn <oleg.synyshyn10@gmail.com>
2024-08-22 17:51:29 +0300

    123

--- case.py
+++ case.py
@@ -1,1 +1,11 @@
+from kivy.uix.gridlayout import product
 #місце для твого коду
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv("IMDB-Movie-Data.csv")
+
+df.info()
+
+df['Rating'].plot(kind= 'hist', bins=5)
+plt.show()
+
